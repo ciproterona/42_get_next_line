c6c6d28bb6aeed293491ab902eb967859093b99e
Ciproterona <eroque-d@student.42sp.org.br>
2026-06-30 18:50:28 -0300

    feat(gnl): implement stash cleaning logic
    - Create clean_stash function to clear extracted line from memory
    - Dynamically allocate new memory for the remaining buffer content
    - Handle EOF scenarios with and without trailing newlines properly

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: eroque-d <eroque-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/14 15:21:25 by eroque-d          #+#    #+#             */
-/*   Updated: 2026/06/30 17:25:51 by eroque-d         ###   ########.fr       */
+/*   Updated: 2026/06/30 18:49:09 by eroque-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,9 +63,31 @@ static char	*extract_line(char *stash)
 	return (buffer);
 }
 
-// char	*clean_stash(char *stash)
-// {
-// }
+static char	*clean_stash(char *stash)
+{
+	int		i;
+	int		j;
+	char	*buffer;
+
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (!stash[i] || !stash[i + 1])
+	{
+		free(stash);
+		return (NULL);
+	}
+	i++;
+	buffer = malloc(ft_strlen(&stash[i]) + 1);
+	if (!buffer)
+		return (NULL);
+	j = 0;
+	while (stash[i])
+		buffer[j++] = stash[i++];
+	buffer[j] = '\0';
+	free(stash);
+	return (buffer);
+}
 
 // char	*get_next_line(int fd)
 // {
